6c507dfcef366a7ad61b2eaaacda4436279ba0c5
eliot-akira <me@eliotakira.com>
2020-02-02 05:02:36 +0100

    Add tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,9 +1,26 @@
-import { test, runTests } from '../index'
+import { test, runTests, testGroup, runTestGroups } from '../index'
 
-test('Tester', it => {
+test('testa', async (it, is) => {
 
-  it('exists', test != null)
+  console.log('---')
+  testGroup('Test group', async (test) => {
 
+    test('Feature name', (it, is) => {
+
+      it('exists', true)
+      it('is true', false)
+
+      const obj1 = { key: 'value' }
+      const obj2 = { key: 'value' }
+
+      it('has deep-strict equal values', is(obj1, obj2))
+    })
+  })
+
+  const { success, fail } = await runTestGroups()
+  console.log('---')
+
+  it('Runs tests', is(fail, 1), { success, fail })
 })
 
 runTests()
